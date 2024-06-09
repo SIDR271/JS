fdc4efa5829b14ea0714b5bb579bbcc96f0a4669
SIDR271 <sligloclassy9456710114@gmail.com>
2024-06-09 22:12:29 +0000

    Add 02_dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treats the written JS code as newer version
+
+// alert (3 + 3) // will not work as we are using node.js not browser
+
+// number
+// bigint
+// string = ""
+// boolean = true/false
+// null = standalone value, it is an object
+// undefined = value is not defined
+// symbol = unique
+// object
+
+//non-primitive
+// object
+
+let age = 24
+
+console.log(typeof age)
+
+console.log(typeof null)
+
+console.log(undefined) // undefined
