c78c0f41386d350c6fa9e02d3e7965dfaa4272bf
SIDR271 <sligloclassy9456710114@gmail.com>
2024-09-14 10:59:54 +0000

    Add Object de-structure and JSON Object

---
+++ 01_Basics/14_Objects de-structure and JSON API.js
@@ -0,0 +1,25 @@
+const course = {
+  courseName: "JS_in_depth",
+  price: 1000,
+  validity: "3 years"
+}
+
+const { validity: val } = course;
+
+console.log(val);
+
+// JSON Object is generally the response of an API
+
+// {
+//   "name":"Siddharth",
+//   "age":23,
+//   "gender":"Male"
+// }
+
+// Array of objects can also be response of an API
+[
+  {},
+  {},
+  {}
+]
+
