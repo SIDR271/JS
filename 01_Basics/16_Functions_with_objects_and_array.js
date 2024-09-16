6316e4bc61134bcafbd59d89cccdb76eb95bf643
SIDR271 <sligloclassy9456710114@gmail.com>
2024-09-16 16:08:08 +0000

    Add function with objects and arrays

---
+++ 01_Basics/16_Functions_with_objects_and_array.js
@@ -0,0 +1,21 @@
+function calculateCartPrice(val1,...num1){
+  return num1
+}
+
+console.log(calculateCartPrice(200,400,480));
+
+function handleObject(myObj){
+  console.log(`Username is ${myObj.username} and age is ${myObj.age}`);
+}
+
+handleObject({
+  username:"Siddharth",
+  age:144
+})
+
+function handleArray(arr){
+  return arr[1]
+}
+
+console.log(handleArray([89,20,90]));
+
